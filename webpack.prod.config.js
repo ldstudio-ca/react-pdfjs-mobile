2879cca84652ad85114eba4865b4893c08120aff
Larry Rivera <larry@ldstudio.com>
2016-10-08 19:42:07 -0700

    add deploy webpack config

---
+++ webpack.prod.config.js
@@ -0,0 +1,64 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+};
+var reactDOMExternal = {
+  root: 'ReactDOM',
+  commonjs2: 'react-dom',
+  commonjs: 'react-dom',
+  amd: 'react-dom'
+};
+
+module.exports = {
+
+  entry: {
+    'react-pdfjs-mobile': './src/index.js',
+    'react-pdfjs-mobile.min': './src/index.js'
+  },
+
+  externals: {
+    'react': reactExternal,
+    'react-dom': reactDOMExternal
+  },
+
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: 'dist',
+    publicPath: '/',
+    libraryTarget: 'umd',
+    library: 'ReactPDFJS'
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
+    ]
+  }
+
+};
