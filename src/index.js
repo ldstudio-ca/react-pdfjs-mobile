d73d64566bee0c2964ab3039355b64da51144a5b
Larry Rivera <larry@ldstudio.com>
2016-10-10 21:25:06 -0700

    adding dist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-
-ReactDOM.render(<App url="../how-to-setup.pdf" />, document.getElementById('root'));
-
-
+export default App;
